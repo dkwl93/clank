29d1bc9d558ae779b511cc0aee55a2a47f66e88b
Dan Lee <dkwl93@gmail.com>
2019-04-23 22:42:14 +1200

    more stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { WebClient } = require('@slack/web-api');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 
-const { postMessage } = require('./postMessage');
+const { updateLabel } = require('./postMessage');
 
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 const CHANNEL = 'CJ3LQT2EL';
@@ -30,26 +30,33 @@ app.get('/', (req, res) => {
 app.post('/', async (req, res) => {
   const actionType = _.get(req, ['body', 'action']);
 
+  // Can it not have an action??
   if (actionType) {
     if (actionType ==='labeled') {
       const labelName = _.get(req, 'body.label.name');
-      const prTitle = _.get(req, 'body.pull_request.title');
-      const prNumber = _.get(req, 'body.pull_request.number');
-      const prUrl = _.get(req, 'body.pull_request.url');
-      const color = _.get(req, 'body.label.color');
-      await postMessage(labelName, prTitle, prNumber, prUrl, color, CHANNEL, web);
-      res.send(200);
-      return;
+
+      // Only post to slack if labels are relevant
+      if (labelName === 'Ready for Review' || labelName === 'Question') {
+        // Get the things I need
+        const prTitle = _.get(req, 'body.pull_request.title');
+        const prNumber = _.get(req, 'body.pull_request.number');
+        const prUrl = _.get(req, 'body.pull_request.url');
+        const labelColor = _.get(req, 'body.label.color');
+
+        // Post to slack
+        await updateLabel(labelName, prTitle, prNumber, prUrl, labelColor, CHANNEL, web);
+
+        // Let GitHub know everything went well
+        return res.sendStatus(200);
+      }
     }
   }
 
-  res.send(400);
+  res.sendStatus(400);
 });
 
-app.get('/test', (req, res) => {
-  res.send(JSON.stringify({
-    Message: 'GET /test SUCCESS'
-  }))
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
 })
 
 app.listen(port, () => {
--- postMessage.js
+++ postMessage.js
@@ -1,12 +1,22 @@
-const postMessage = async (label, prTitle, prNumber, prUrl, color, channelId, web) => {
+const _ = require('lodash');
+
+const SLACK_USER_MAP = {
+  djdan: 'U6TQSM5FG',
+}
+
+const getSlackId = (slackUsername) =>
+  _.get(SLACK_USER_MAP, slackUsername, slackUsername);
+
+const updateLabel = async (labelName, prTitle, prNumber, prUrl, labelColor, channelId, web) => {
   try {
     await web.chat.postMessage({
       channel: channelId,
+      parse: 'full',
       attachments: [
         {
-          "pretext": `@djdan has marked #${prNumber} by @djdan as ${label}`,
-          "fallback": `@djdan has marked #${prNumber} as ${label}`,
-          "color": `#${color}`,
+          "pretext": `@djdan has marked #${prNumber} by @djdan as ${labelName}`,
+          "fallback": `@djdan has marked #${prNumber} as ${labelName}`,
+          "color": `#${labelColor}`,
           "title": `#${prNumber}`,
           "title_link": prUrl,
           "text": `${prTitle}`
@@ -23,5 +33,5 @@ const postMessage = async (label, prTitle, prNumber, prUrl, color, channelId, we
 }
 
 module.exports = {
-  postMessage,
+  updateLabel,
 }
